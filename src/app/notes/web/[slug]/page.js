e9cd12586e51478c25cef5a06979744cbde0b3ea
Shreyas Kaushik <shreyas.skp@gmail.com>
2024-09-17 19:07:53 +0530

    Design Changes

--- src/app/notes/web/[slug]/page.js
+++ src/app/notes/web/[slug]/page.js
@@ -32,24 +32,26 @@ export default async function WebNotes({ params }) {
   }
 
   return (
-    <div>
-      <h1>{doc.title}</h1>
-      <p>{doc.description}</p>
+    <div className="max-w-[1204px] mx-auto px-4 md:px-0 py-10">
+      <h1 className="scroll-m-20 text-4xl font-medium tracking-tight lg:text-6xl animate-fade-up animate-duration-700 delay-700">{doc.title}</h1>
+      <p className="mb-8 text-lg text-gray-200">{doc.description}</p>
+      <br></br>
       <PortableText value={doc.body} components={{
         types: {
           code: ({ value }) => (
-            <pre>
+            <pre className="bg-gray-800 text-white p-4 rounded-lg overflow-x-auto mb-6">
               <code>{value.code}</code>
             </pre>
           ),
           image: ({ value }) => (
-            <figure>
+            <figure className="mb-6"> 
               <img
                 src={urlFor(value.asset).width(800).url()} 
                 alt={value.caption || 'Image'}
                 style={{ maxWidth: '100%', height: 'auto' }}
+                className="mx-auto rounded-lg"
               />
-              {value.caption && <figcaption>Diag: {value.caption}</figcaption>}
+              {value.caption && <figcaption className="text-center text-sm text-gray-400 mt-2">Diag: {value.caption}</figcaption>}
             </figure>
           )
         }
